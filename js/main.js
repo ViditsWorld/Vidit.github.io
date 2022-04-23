c4d44920b53591586be91133e9f28a6a7c355724
ViditsWorld <104258065+ViditsWorld@users.noreply.github.com>
2022-04-23 18:12:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ $(window).on('load', function() {
 		TYPED JS
 	--------------------*/
 	$(".element").typed({
-		strings: ["I'm Vidit", "Web Designer", "From India"],
+		strings: ["I'm Vidit Chowdary", "Narayana School ", "From Chennai, India"],
 		typeSpeed: 10,
 		loop:true,
 		backDelay: 2000
